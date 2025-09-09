a746aa5aa6bad9fc9201f8e99f6feb68cd5a6aca
jdhimanbe24-cmyk <jdhiman_be24@thapar.edu>
2025-09-09 10:42:38 +0530

    Update Q4b_ass2.cpp

--- Lab_Assignment2/Q4b_ass2.cpp
+++ Lab_Assignment2/Q4b_ass2.cpp
@@ -1,12 +1,21 @@
+//reverse a string 
 #include <iostream>
 #include <string>
-#include <algorithm>
 using namespace std;
 
 int main() {
     string s;
     cout << "Enter string: ";
     cin >> s;
-    reverse(s.begin(), s.end());
+
+    // manual reverse
+    int n = s.length();
+    for (int i = 0; i < n / 2; i++) {
+        char temp = s[i];
+        s[i] = s[n - i - 1];
+        s[n - i - 1] = temp;
+    }
+
     cout << "Reversed: " << s << endl;
+    return 0;
 }
