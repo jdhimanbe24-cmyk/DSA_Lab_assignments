c785a1df61cb4fcbf565e8686a710c8cae0fc360
jdhimanbe24-cmyk <jdhiman_be24@thapar.edu>
2025-09-09 10:28:54 +0530

    Update Q1_ass1.cpp

--- Lab_Assignment1/Q1_ass1.cpp
+++ Lab_Assignment1/Q1_ass1.cpp
@@ -1,3 +1,4 @@
+// develop a menu driven program to 1.CREATE 2.DISPLAY 3.INSERT 4.DELETE 5.LINEAR SEARCH 6.EXIT
 #include <iostream>
 using namespace std;
 
@@ -68,4 +69,5 @@ int main() {
         }
     } while (choice != 6);
     return 0;
+
 }
