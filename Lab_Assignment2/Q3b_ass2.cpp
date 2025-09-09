9e20570dd1ca32b19ea419f74101bc416cd63c23
jdhimanbe24-cmyk <jdhiman_be24@thapar.edu>
2025-09-09 10:39:01 +0530

    Update Q3b_ass2.cpp

--- Lab_Assignment2/Q3b_ass2.cpp
+++ Lab_Assignment2/Q3b_ass2.cpp
@@ -1,3 +1,4 @@
+// find a missing number using binary search.
 #include <iostream>
 using namespace std;
 
@@ -16,3 +17,4 @@ int main() {
     int n = 6;
     cout << "Missing number: " << findMissingBinary(arr, n) << endl;
 }
+
