c73730e2f41b9e7c37a67973a7b3c210d10069cf
jdhimanbe24-cmyk <jdhiman_be24@thapar.edu>
2025-09-09 11:03:29 +0530

    Update Q8_ass2.cpp

--- Lab_Assignment2/Q8_ass2.cpp
+++ Lab_Assignment2/Q8_ass2.cpp
@@ -1,5 +1,5 @@
+// to count total number of distinct elements in an array of length n.
 #include <iostream>
-#include <unordered_set>
 using namespace std;
 
 int main() {
@@ -8,10 +8,21 @@ int main() {
     cin >> n;
     int arr[n];
     cout << "Enter elements: ";
-    for (int i=0;i<n;i++) cin >> arr[i];
+    for (int i = 0; i < n; i++) cin >> arr[i];
 
-    unordered_set<int> s;
-    for(int i=0;i<n;i++) s.insert(arr[i]);
+    int distinct = 0;  // counter for distinct elements
+    for (int i = 0; i < n; i++) {
+        bool isDuplicate = false;
+        // check if arr[i] appeared before
+        for (int j = 0; j < i; j++) {
+            if (arr[i] == arr[j]) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (!isDuplicate) distinct++;
+    }
 
-    cout << "Distinct count: " << s.size() << endl;
+    cout << "Distinct count: " << distinct << endl;
+    return 0;
 }
